7d5cd358afe0b72bfc34f8b90f1840be6fb7ce15
Dan Sullivan <dansullivan@gmail.com>
2022-08-17 22:03:24 -0400

    feat: openpgp support

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
-console.info('First commit.')
+import * as openpgp from 'openpgp';
+import { v4 as uuidv4 } from 'uuid';
+
+const passphrase = uuidv4();
+const { privateKey, publicKey, revocationCertificate } = await openpgp.generateKey({
+    type: 'ecc',
+    curve: 'curve25519',
+    userIDs: [{ name: 'Test User', email: 'test@test.com' }],
+    passphrase,
+    format: 'armored'
+});
+
+console.log(privateKey);
+console.log(publicKey);
+console.log(revocationCertificate);
